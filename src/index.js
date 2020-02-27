3cc36519d060d07f5950d4440fc49bab1960f2fb
Hom <b96208015@gmail.com>
2020-02-27 19:13:24 +0800

    set camera

--- src/index.js
+++ src/index.js
@@ -6,11 +6,27 @@ document.addEventListener('DOMContentLoaded', function(event) {
         infoBox : false,
         sceneModePicker: false,
         timeline: false,
-    };    
+    };
     var viewer = new Cesium.Viewer('cesiumContainer', options);
     var scene = viewer.scene;
     var camera = viewer.camera;
     var canvas = scene.canvas;
     var imageryLayers = viewer.imageryLayers;
     var handler = new Cesium.ScreenSpaceEventHandler(canvas);
+
+    camera.setView({
+        destination : Cesium.Cartesian3.fromDegrees(
+            121,
+            23.5,
+            800000
+        )
+    });
+    camera.changed.addEventListener(
+        function() {
+            if (camera._suspendTerrainAdjustment && scene.mode === Cesium.SceneMode.SCENE3D) {
+                camera._suspendTerrainAdjustment = false;
+                camera._adjustHeightForTerrain();
+            }
+        }
+    );
 });
